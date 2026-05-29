7b426a0a764a677ac3746ac89deb8964354399ec
yashaswini2305 <yashaswiniatyam@gmail.com>
2026-05-29 18:30:40 +0530

    Create Pointers in function call-type-1.c

---
+++ Pointers in function call-type-1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void square(int n);
+int main() {
+   int number=4;
+   square(number);
+   printf("number=%d\n",number);
+   return 0;
+}
+//call by value
+void square(int n){
+    n=n*n;
+    printf("square=%d\n",n);
+}
